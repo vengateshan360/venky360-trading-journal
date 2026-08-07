0e2b3f987382fa4d799612e47be7675e729737ee
vengateshan360 <vvenkateshan306@gmail.com>
2026-08-07 09:18:56 +0530

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIza...",
+  authDomain: "venky-360-trader.firebaseapp.com",
+  projectId: "venky-360-trader",
+  storageBucket: "venky-360-trader.firebasestorage.app",
+  messagingSenderId: "65911533116",
+  appId: "1:65911533116:web:66b72848a9d8c3eef75d39",
+  measurementId: "G-LX57KNZM2S"
+};
