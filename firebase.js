69ef8e2d4e258c0177b018fc3733626802e81014
vengateshan360 <vvenkateshan306@gmail.com>
2026-08-07 09:23:21 +0530

    Remove empty line at the beginning of firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-
 // Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
