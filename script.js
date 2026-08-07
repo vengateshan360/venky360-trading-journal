5f828c383b48c91b1b2225259c7e737bfebb49ea
vengateshan360 <vvenkateshan306@gmail.com>
2026-08-07 09:30:18 +0530

    Implement user registration, login, and logout functions

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ import { firebaseConfig } from "./firebase.js";
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const tradesRef = collection(db, "trades");
+import { auth, db } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Register
+window.registerUser = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Account Created Successfully!");
+    window.location.href = "trade.html";
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+// Login
+window.loginUser = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login Successful!");
+    window.location.href = "trade.html";
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+// Logout
+window.logoutUser = async function () {
+  await signOut(auth);
+  window.location.href = "login.html";
+};
