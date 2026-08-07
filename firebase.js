1aa1ff0c58e0e0ca771fecd626738f724dd36aea
vengateshan360 <vvenkateshan306@gmail.com>
2026-08-07 09:22:53 +0530

    Update Firebase API key in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,12 @@
+
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIza...",
+  apiKey: "AIzaSyAQN-FzfxWDFXHLiAecLumJABAawt9hRjs",
   authDomain: "venky-360-trader.firebaseapp.com",
   projectId: "venky-360-trader",
   storageBucket: "venky-360-trader.firebasestorage.app",
@@ -7,3 +14,10 @@ const firebaseConfig = {
   appId: "1:65911533116:web:66b72848a9d8c3eef75d39",
   measurementId: "G-LX57KNZM2S"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Auth & Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
